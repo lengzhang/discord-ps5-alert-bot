16d9ef285b28cac12d01b73cd5fc16a3db4b4d89
Leng <lengzhang@live.com>
2020-12-09 09:21:04 -0800

    init node.js and discord

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const { DISCORD_TOKEN } = process.env
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', (msg) => {
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+})
+
+client.login(DISCORD_TOKEN)
